0ca8d2f5906f801a6e6306bd46b11834a362ef52
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2022-12-17 12:04:07 +0500

    test

---
+++ src/Components/NavigateLink/NavigateLink.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import ".NavigateLink.css"
+const NavigateLink = () => {
+    return (
+      <>
+ 
+      </>
+    );
+};
+
+export default NavigateLink;
