2f7b6116630fae2bf45ce61809c4f3c8a54f0c40
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2022-12-19 15:32:26 +0500

    testing

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,9 +8,9 @@ import { Outlet } from "react-router-dom";
 
 import "./style.scss";
 import MainCard from "../../Components/MainCard/MainCard";
+import HomenavigateLink from "./HomeNavigatePages/HomenavigateLink";
 const Home = () => {
   return (
-<<<<<<< HEAD
     <main>
       <div className="row">
         <div className="col-8">
@@ -20,8 +20,7 @@ const Home = () => {
             date="Tuesday, 2 Feb 2021"
           />
           <NavigateLinks/>
-              <Outlet />
-        
+              <HomenavigateLink/>
           {/* Muhammadamin */}
           <div className="row WrapperCard">
             <div className="col-6">
@@ -38,44 +37,14 @@ const Home = () => {
               <MainCard />
             </div>
           </div>
-=======
-    <div>
-      <main>
-        <div className="row m-0 p-0">
-          <div className="col-8 ">
-            <PageTitle
-              input={"input put"}
-              title="Jaegar Resto"
-              date="Tuesday, 2 Feb 2021"
-            />
-
-            {/* Muhammadamin */}
-            <div className="row WrapperCard">
-              <div className="col-6">
-                <h3>Choose Dishes</h3>
-              </div>
-              <div className="col-6 text-end">
-                <select name="" id="">
-                  <option>Dine In</option>
-                  <option>oaAwsnja</option>
-                  <option>csadkbhsd</option>
-                </select>
-              </div>
-              <div className="row">
-                <MainCard />
-              </div>
-            </div>
-          </div>
-
+        </div>
+        
           <div className="col-4 p-4 ordersWrapp1">
             <div className="ordersWrapp2">
               <OrdersHome />
             </div>
           </div>
->>>>>>> be930103370d274afc4895b77dc01b488d44d290
-        </div>
-        <div className="col-4 p-5 bg-info"></div>
-      </div>
+   </div>
     </main>
   );
 
---
+++ src/Pages/Home/HomeNavigatePages/HomenavigateLink.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+const HomenavigateLink = () => {
+    return (
+        <>
+            <Outlet/>
+        </>
+    );
+};
+
+export default HomenavigateLink;
