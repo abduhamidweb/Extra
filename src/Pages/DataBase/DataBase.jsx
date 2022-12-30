ca4aa6d68dfa9c3935de8ccb8250735a9780e8d5
Abduhamid <abduhamidbotirovweb@gmail.com>
2022-12-30 16:46:00 +0500

    Test

--- src/Pages/DataBase/DataBase.jsx
+++ src/Pages/DataBase/DataBase.jsx
@@ -2,74 +2,72 @@ import React from "react";
 import PageTitle from "../../Components/PageTitile/PageTitle";
 import Dashbaord from "../../Components/DashboardCard/DashboardTop";
 import MostOrder from "../../Components/MostOrder/MostOrder";
-import "./DataBase.scss"
+import "./DataBase.scss";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import Users from "../../Components/Users/Users";
-import MostTypeCircle from "../../Components/MostTypeCircle/MostTypeCircle"
+import MostTypeCircle from "../../Components/MostTypeCircle/MostTypeCircle";
 
 const DataBase = () => {
-  const [valueMeal,setValueMeal]=useState("today")
-const orderData=[]
-    let OrderCard = [
-      {
-        MostTitle: "Spicy seasoned seafood noodles",
-        MostImg:
-          "https://www.themealdb.com/images/media/meals/vptqpw1511798500.jpg",
-        MostDescription: "200 dishes ordered",
-        id: 1,
-        Category: "today",
-      },
-      {
-        MostTitle: "Salted pasta with mushroom sauce",
-        MostImg:
-          "https://www.themealdb.com/images/media/meals/a15wsa1614349126.jpg",
-        MostDescription: "120 dishes ordered",
-        id: 2,
-        Category: "today",
-      },
-      {
-        MostTitle: "Beef dumpling in hot and sour soup",
-        MostImg:
-          "https://www.themealdb.com/images/media/meals/ysxwuq1487323065.jpg",
-        MostDescription: "80 dishes ordered",
-        id: 3,
-        Category: "today",
-      },
-      {
-        MostTitle: "Salted pasta with mushroom sauce",
-        MostImg: "https://www.themealdb.com/images/media/meals/1548772327.jpg",
-        MostDescription: "80 dishes ordered",
-        id: 4,
-        Category: "lastday",
-      },
-      {
-        MostTitle: "Beef dumpling in hot and sour soup",
-        MostImg: "https://www.themealdb.com/images/media/meals/1520084413.jpg",
-        MostDescription: "120 dishes ordered",
-        id: 5,
-        Category: "lastday",
-      },
-      {
-        MostTitle: "Spicy seasoned seafood noodles",
-        MostImg:
-          "https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
-        MostDescription: "30 dishes ordered",
-        id: 6,
-        Category: "lastday",
-      },
-    ];
+  const [valueMeal, setValueMeal] = useState("today");
+  const orderData = [];
+  let OrderCard = [
+    {
+      MostTitle: "Spicy seasoned seafood noodles",
+      MostImg:
+        "https://www.themealdb.com/images/media/meals/vptqpw1511798500.jpg",
+      MostDescription: "200 dishes ordered",
+      id: 1,
+      Category: "today",
+    },
+    {
+      MostTitle: "Salted pasta with mushroom sauce",
+      MostImg:
+        "https://www.themealdb.com/images/media/meals/a15wsa1614349126.jpg",
+      MostDescription: "120 dishes ordered",
+      id: 2,
+      Category: "today",
+    },
+    {
+      MostTitle: "Beef dumpling in hot and sour soup",
+      MostImg:
+        "https://www.themealdb.com/images/media/meals/ysxwuq1487323065.jpg",
+      MostDescription: "80 dishes ordered",
+      id: 3,
+      Category: "today",
+    },
+    {
+      MostTitle: "Salted pasta with mushroom sauce",
+      MostImg: "https://www.themealdb.com/images/media/meals/1548772327.jpg",
+      MostDescription: "80 dishes ordered",
+      id: 4,
+      Category: "lastday",
+    },
+    {
+      MostTitle: "Beef dumpling in hot and sour soup",
+      MostImg: "https://www.themealdb.com/images/media/meals/1520084413.jpg",
+      MostDescription: "120 dishes ordered",
+      id: 5,
+      Category: "lastday",
+    },
+    {
+      MostTitle: "Spicy seasoned seafood noodles",
+      MostImg:
+        "https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
+      MostDescription: "30 dishes ordered",
+      id: 6,
+      Category: "lastday",
+    },
+  ];
 
-  OrderCard.map(i => {
+  OrderCard.map((i) => {
     if (i.Category == valueMeal) {
       orderData.push(i);
-    } 
-})
+    }
+  });
 
-
-  
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <>
       <div className="row m-0 p-0">
@@ -129,7 +127,7 @@ const orderData=[]
                 </select>
               </span>
             </div>
-            <hr className="my-1"/>
+            <hr className="my-1" />
             {orderData.map((el) => {
               return <MostOrder data={el} />;
             })}
@@ -145,44 +143,41 @@ const orderData=[]
 
           <div className="mostTypeCircleWrapp">
             <div className="topMostTypeWrapp d-flex mb-2 justify-content-between align-item-center">
-              <h2 className="topMostTypeTitle m-0">
-              Most Type of Order
-              </h2>
-           <span className="mostCircleSelectWrapp">
-            <select id="circleSelect" className="circleSelect">
-              <option value="Tooday">Tooday</option>
-              <option value="Lastday">Lastday</option>
-            </select>
-           </span>
+              <h2 className="topMostTypeTitle m-0">Most Type of Order</h2>
+              <span className="mostCircleSelectWrapp">
+                <select id="circleSelect" className="circleSelect">
+                  <option value="Tooday">Tooday</option>
+                  <option value="Lastday">Lastday</option>
+                </select>
+              </span>
             </div>
-            <hr className="m-0"/>
+            <hr className="m-0" />
 
-<div className="infoCircleWrapp d-flex alig-items-center">
-  <div className="circleWrapp">
-<MostTypeCircle/>
-  </div>
+            <div className="infoCircleWrapp d-flex alig-items-center">
+              <div className="circleWrapp">
+                <MostTypeCircle />
+              </div>
 
-  <div className="circleInfoText px-2 py-3 d-flex flex-column ms-3">
-<div className="circleInfoCard my-1 Dinein">
-  <h3 className="cirleInfoTitle m-0">Dine In</h3>
-  <p className="circleInfoDesc m-0">200 customers</p>
-</div>
-<div className="circleInfoCard my-1 Togo">
-  <h3 className="cirleInfoTitle m-0">To Go</h3>
-  <p className="circleInfoDesc m-0">124 customers</p>
-</div>
-<div className="circleInfoCard my-1 Deliver">
-  <h3 className="cirleInfoTitle m-0">Delivery</h3>
-  <p className="circleInfoDesc m-0">264 customers</p>
-</div>
-  </div>
-</div>
-
+              <div className="circleInfoText px-2 py-3 d-flex flex-column ms-3">
+                <div className="circleInfoCard my-1 Dinein">
+                  <h3 className="cirleInfoTitle m-0">Dine In</h3>
+                  <p className="circleInfoDesc m-0">200 customers</p>
+                </div>
+                <div className="circleInfoCard my-1 Togo">
+                  <h3 className="cirleInfoTitle m-0">To Go</h3>
+                  <p className="circleInfoDesc m-0">124 customers</p>
+                </div>
+                <div className="circleInfoCard my-1 Deliver">
+                  <h3 className="cirleInfoTitle m-0">Delivery</h3>
+                  <p className="circleInfoDesc m-0">264 customers</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </>
   );
-  }
+};
 
 export default DataBase;
